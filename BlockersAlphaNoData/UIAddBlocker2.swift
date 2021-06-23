7b9c1ab39bad7fdec372a4e397aaff50af164035
Christina <myway2027@gmail.com>
2021-06-24 00:27:39 +0900

    0103

--- BlockersAlphaNoData/UIAddBlocker2.swift
+++ BlockersAlphaNoData/UIAddBlocker2.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct UIAddBlocker2: View {
+    
+    @State private var name = ""
+    
     var body: some View {
         VStack {
+            CustomText(text: "예산 블럭의 이름을 작성해주세요.", size: 20, weight: .semibold, design: .default, color: .black)
+            
+            ZStack {
+                // # TODO: replace text to shape with specified size
+                Text("                                                            ")
+                    .padding(.all, 20)
+                    .background(Color.green)
+                    .opacity(0.4)
+                
+                TextField("Blocker Name: ", text: $name)
+                    .offset(x: 50, y: 0)
+            }
+            
+            NavigationLink(
+                destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/,
+                label: {
+                    CustomText(text: "다음", size: 15, weight: .semibold, design: .default, color: .white)
+                        .padding(.all, 10)
+                        .background(Color.green.opacity(0.8))
+                })
+
             
         }
     }
