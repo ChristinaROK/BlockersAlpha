c532aaf9aaaedd9a18db6f892fed16c8294c7ad3
Christina <myway2027@gmail.com>
2021-06-24 23:50:12 +0900

    0101

---
+++ BlockersAlphaNoData/UIAddBlocker3.swift
@@ -0,0 +1,20 @@
+//
+//  UIAddBlocker3.swift
+//  BlockersAlphaNoData
+//
+//  Created by ssj on 2021/06/24.
+//
+
+import SwiftUI
+
+struct UIAddBlocker3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UIAddBlocker3_Previews: PreviewProvider {
+    static var previews: some View {
+        UIAddBlocker3()
+    }
+}
